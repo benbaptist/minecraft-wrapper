4ca2244790c8e00a20ca7ddc1fd138ebf3c3f4df
suresttexas00 <suresttexas00@live.com>
2017-02-03 20:12:14 -0500

    edit base.py documentation

--- wrapper/api/base.py
+++ wrapper/api/base.py
@@ -296,10 +296,10 @@ class API:
         data your plugin will remember across reboots.
 
         :NOTE: This method is somewhat different after Wrapper
-        version 0.10.1 (build 182).  The storage object is no longer
-        a data object itself; It is a manager used for controlling
-        the saving of the object data.  The actual data is contained
-        in Dictionary subitem 'Data'
+         version 0.10.1 (build 182).  The storage object is no longer
+         a data object itself; It is a manager used for controlling
+         the saving of the object data.  The actual data is contained
+         in Dictionary subitem 'Data'
 
         ..
 
