88fc6d6ef90a8f16a1184f2ce6528cd60083a44d
Alex <alex.fruscella@gmail.com>
2018-04-07 15:51:56 -0500

    Update DiscRelay.py

--- submitted-plugins/DiscRelay.py
+++ submitted-plugins/DiscRelay.py
@@ -22,22 +22,27 @@ class Main:
 
         self.api.registerEvent("player.message", self.playerMessage)
         self.api.registerEvent("player.login", self.login)
-        self.online_players = {}
+        
+        self.discord_storage = self.api.getStorage("discord")
+        self.player_data = self.discord_storage.Data
 
         self.api.registerCommand("discord", self._toggle, None)
         self.api.registerHelp("DiscordRelay","sends and receives messages from Discord",
-                              [("/discord toggle", "toggles whether you want to communicate with the Discord server", None)])
+                              [("/discord toggle",
+                                "toggles whether you want to communicate with the Discord server",
+                                None)])
 
         @self.client.event
         async def on_message(message):
             for p in self.api.minecraft.getPlayers():
-                if self.online_players[p.uuid] == True:
+                if self.player_data[p.uuid] == True:
                     p.message(message)
 
         self.client.start("token")
 
     def onDisable(self):
         self.client.logout()
+        self.discord_storage.close()
         pass
 
     def playerMessage(self, payload):
@@ -46,24 +51,25 @@ class Main:
 
         @self.client.event
         async def discMessage():
-            if payload["player"].uuid == True:
+            if self.player_data[player.uuid] == True:
                 await self.client.edit_profile(username = playerName)
                 self.client.send_message(self.sendChannel, payload["message"])
 
         discMessage()
 
     def login(self, payload):
-        self.online_players[payload["player"].uuid] = True
+        if payload["player"].uuid not in self.player_data:
+            self.player_data[payload["player"].uuid] = True
         pass
 
     def _toggle(self, player, args):
         if args[0] == "toggle":
-            if self.online_players[player.uuid] == True:
-                self.online_players[player.uuid] = False
+            if self.player_data[player.uuid] == True:
+                self.player_data[player.uuid] = False
                 player.message("Disabled message relay with Discord.")
             else:
-                self.online_players[player.uuid] = True
+                self.player_data[player.uuid] = True
                 player.message("Enabled message relay with Discord.")
         else:
             player.message("Wrong argument: type 'toggle'")
-        pass
+        
