7e3cea85a9c14eb00c985bedf91dd984d16643b4
Alex <alex.fruscella@gmail.com>
2018-04-07 12:00:08 -0500

    Update DiscRelay.py

--- submitted-plugins/DiscRelay.py
+++ submitted-plugins/DiscRelay.py
@@ -1,5 +1,6 @@
 AUTHOR = "PurePi"
 VERSION = (0, 1, 0)
+NAME = "Discord Relay"
 SUMMARY = "Minecraft-to-Discord and Discord-to-Minecraft chat relay"
 DESCRIPTION = """ This plugin sends all players' messages to a Discord server
 and broadcasts messages from the Discord server to the Minecraft server
@@ -16,17 +17,23 @@ class Main:
 
     def onEnable(self):
         self.client = discord.Client()
-        self.server = discord.Server(kwargs={"id": "serverID"})
-
-        self.sendChannel = discord.Channel(kwargs={"name": "channelName",
-                                                   "server": self.server,
-                                                   "id": "channelID"})
+        self.server = discord.Server(id = "serverID")
+        self.sendChannel = discord.Channel(name = "channelName", server = self.server, id = "channelID")
 
         self.api.registerEvent("player.message", self.playerMessage)
+        self.api.registerEvent("player.login", self.login)
+        self.online_players = {}
+
+        self.api.registerCommand("discord", self._toggle, None)
+        self.api.registerHelp("DiscordRelay","sends and receives messages from Discord",
+                              [("/discord toggle", "toggles whether you want to communicate with the Discord server", None)])
 
         @self.client.event
-        async def on_message(self, message):
-            self.api.minecraft.broadcast(message)
+        async def on_message(message):
+            for p in self.api.minecraft.getPlayers():
+                if p.uuid in self.online_players:
+                    p.message(message)
+            #(p.message(message, 0) for p in self.api.minecraft.getPlayers if p.uuid in self.online_players)
 
         self.client.start("token")
 
@@ -40,7 +47,24 @@ class Main:
 
         @self.client.event
         async def discMessage():
-            await self.client.edit_profile(fields = {"username": playerName})
-            await self.client.send_message(self.sendChannel, payload["message"])
+            if payload["player"].uuid == True:
+                await self.client.edit_profile(username = playerName)
+                self.client.send_message(self.sendChannel, payload["message"])
 
         discMessage()
+
+    def login(self, payload):
+        self.online_players[payload["player"].uuid] = True
+        pass
+
+    def _toggle(self, player, args):
+        if args[0] == "toggle":
+            if self.online_players[player.uuid] == True:
+                self.online_players[player.uuid] = False
+                player.message("Disabled message relay with Discord.")
+            else:
+                self.online_players[player.uuid] = True
+                player.message("Enabled message relay with Discord.")
+        else:
+            player.message("Wrong argument: type 'toggle'")
+        pass
